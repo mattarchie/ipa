e529af9ec63b92be5029e09de535f8da9c8b087e
Ben O'Halloran <bohallor@u.rochester.edu>
2016-11-03 13:03:18 -0400

    [tests] multi test uniform distribution bt classes

--- tests/multi_alloc.c
+++ tests/multi_alloc.c
@@ -8,14 +8,21 @@ extern void * noomr_free(void *);
 bool speculating() {
   return false;
 }
-#define NUM_ROUNDS 5000
+#define NUM_ROUNDS 50000
 
-size_t rand_num_ints(){
-  int max;
-  for(max = 1; sizeof(int) * max < MAX_SIZE - sizeof(block_t); max++) {
-    ;
-  }
-  return rand() % (max + 1 - 0) + 0;
+// Random number generation based off of http://www.azillionmonkeys.com/qed/random.html
+#define RS_SCALE (1.0 / (1.0 + RAND_MAX))
+
+size_t random_class () {
+    double d;
+    do {
+       d = (((rand () * RS_SCALE) + rand ()) * RS_SCALE + rand ()) * RS_SCALE;
+    } while (d >= 1); /* Round off */
+    return d * NUM_CLASSES;
+}
+
+size_t uniform_size_class() {
+  return ALIGN(CLASS_TO_SIZE(random_class()) - sizeof(block_t));
 }
 
 int main() {
@@ -29,7 +36,7 @@ int main() {
   int * ptrs[NUM_ROUNDS] = {NULL};
 
   for (rnd = 0; rnd < NUM_ROUNDS; rnd++) {
-    int * payload = noomr_malloc(rand_num_ints());
+    int * payload = noomr_malloc(uniform_size_class());
     ptrs[rnd] = payload;
     for (check = 0; check < rnd; check++) {
       if (ptrs[check] == payload) {
