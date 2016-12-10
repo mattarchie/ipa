814fca13d7decdae92e6c6bf6eade59bc2d5bd36
Ben O'Halloran <bohallor@u.rochester.edu>
2016-12-10 00:32:22 -0500

    all mixed test

---
+++ tests/all_tests.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "noomr.h"
+#include "dummy.h"
+
+#define NUM_ROUNDS 5000
+
+// Random number generation based off of http://www.azillionmonkeys.com/qed/random.html
+#define RS_SCALE (1.0 / (1.0 + RAND_MAX))
+
+size_t random_class () {
+    double d;
+    do {
+       d = (((rand () * RS_SCALE) + rand ()) * RS_SCALE + rand ()) * RS_SCALE;
+    } while (d >= 1); /* Round off */
+    return d * (NUM_CLASSES + 1);
+}
+
+size_t uniform_size_class() {
+  size_t klass = random_class();
+  if (klass >= NUM_CLASSES) {
+    return MAX_SIZE + sizeof(block_t) + 1;
+  }
+  return ALIGN(CLASS_TO_SIZE(klass) - sizeof(block_t));
+}
+
+int main() {
+  int rnd, check;
+  size_t alloc_size;
+
+  srand(0);
+
+  int * ptrs[NUM_ROUNDS] = {NULL};
+
+  for (rnd = 0; rnd < NUM_ROUNDS; rnd++) {
+    alloc_size = uniform_size_class();
+    int * payload = noomr_malloc(alloc_size);
+    assert(noomr_usable_space(payload) >= alloc_size);
+    ptrs[rnd] = payload;
+    for (check = 0; check < rnd; check++) {
+      if (ptrs[check] == payload) {
+        fprintf(stderr, "Duplicate allocation found");
+        exit(-1);
+      }
+    }
+    *payload = 42;
+  }
+  printf("Basic allocation test passed\n");
+  print_noomr_stats();
+}
