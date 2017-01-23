d746e9db306c89effe8a2431f05f0cddbe435789
Ben O'Halloran <ben@ohalloran.io>
2017-01-23 00:56:52 -0500

    [test] Reduce iterations on no_overlap test

--- tests/no_overlap.c
+++ tests/no_overlap.c
@@ -3,7 +3,7 @@
 #include "noomr.h"
 #include "dummy.h"
 
-#define NUM_ROUNDS 1000
+#define NUM_ROUNDS 50
 
 size_t class_for_rnd(int rnd) {
   return ALIGN(CLASS_TO_SIZE(rnd % NUM_CLASSES) - sizeof(block_t));
